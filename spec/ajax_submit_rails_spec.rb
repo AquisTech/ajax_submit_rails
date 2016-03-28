e1dbdd9ac5b5357a8a15c0e5ca19fb944163a2a6
Anand Bait <anand.bait@gmail.com>
2016-03-29 00:40:21 +0530

    Initial commit for ajax_submit_rails

---
+++ spec/ajax_submit_rails_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+describe AjaxSubmitRails do
+
+  it 'has a version number' do
+
+    expect(AjaxSubmitRails::VERSION).not_to be nil
+
+  end
+
+  it 'does something useful' do
+
+    expect(false).to eq(true)
+
+  end
+
+end
